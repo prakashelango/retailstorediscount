af35ecbcfab582435de69218a0362533901e3991
prakashelango <prakashcse100@gmail.com>
2019-08-27 19:40:06 +0530

    Base Implementation

---
+++ src/test/java/com/retailstore/retailsales/RetailsalesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.retailstore.retailsales;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RetailsalesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
