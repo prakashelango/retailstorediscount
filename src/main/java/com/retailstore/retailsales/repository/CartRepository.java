74c24339faec46e2fa1d80e8bb59e97e8869488f
prakashelango <prakashcse100@gmail.com>
2019-08-29 11:11:57 +0530

    Controller Implementation

--- src/main/java/com/retailstore/retailsales/repository/CartRepository.java
+++ src/main/java/com/retailstore/retailsales/repository/CartRepository.java
@@ -3,5 +3,9 @@ package com.retailstore.retailsales.repository;
 import com.retailstore.retailsales.model.cart.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    List<Cart> findAllByUserid(long userid);
 }
